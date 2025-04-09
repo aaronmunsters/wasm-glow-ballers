8b4e7974bb41a22f99533f690dddd7bf0b95bf98
aaronmunsters <aaronmunstersbxl@gmail.com>
2025-04-09 22:08:24 +0200

    Allow for conversion + error handling by caller

--- wasm-glow-ballers-js/src/lib.rs
+++ wasm-glow-ballers-js/src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 use wasm_glow_ballers_lib::mapping;
 
 #[wasm_bindgen]
-pub fn analyse(target: Vec<u8>) -> JsValue {
-    serde_wasm_bindgen::to_value(&mapping(&target)).unwrap()
+pub fn analyse(target: Vec<u8>) -> Result<JsValue, JsError> {
+    let map = mapping(&target).map_err(JsError::from)?;
+    Ok(serde_wasm_bindgen::to_value(&map)?)
 }
--- wasm-glow-ballers-lib/src/lib.rs
+++ wasm-glow-ballers-lib/src/lib.rs
@@ -1,14 +1,17 @@
+pub mod error;
+
 use std::collections::HashMap;
 
+use error::Error;
 use walrus::Module;
 
-pub fn mapping(wasm: &[u8]) -> HashMap<String, usize> {
-    let module = Module::from_buffer(wasm).unwrap();
+pub fn mapping(wasm: &[u8]) -> Result<HashMap<String, usize>, Error> {
+    let module = Module::from_buffer(wasm).map_err(|_| Error::ParsingFailed)?;
     let mut mapping = HashMap::new();
     for export in module.exports.iter() {
         if let walrus::ExportItem::Global(id) = export.item {
             mapping.insert(export.name.to_string(), id.index());
         }
     }
-    return mapping;
+    return Ok(mapping);
 }
